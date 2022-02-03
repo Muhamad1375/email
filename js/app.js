52672870bf8c31979d8a2f271af45fc02deeb3a2
Muhamad1375 <Mohammadali.fallahzade@gmail.com>
2022-02-03 18:37:41 +0330

    finished

---
+++ js/app.js
@@ -0,0 +1,122 @@
+/*******************/
+//variables
+/*******************/
+const sendBtn = document.getElementById('sendBtn'),
+      email = document.getElementById('email'),
+      subject = document.getElementById('subject'),
+      message = document.getElementById('message'),
+      resetBtn = document.getElementById('resetBtn'),
+      sendEmailForm = document.getElementById('email-form');
+
+
+
+/*******************/
+//event listeners
+/*******************/
+eventListeners();
+
+function eventListeners() {
+      // app init
+      document.addEventListener('DOMContentLoaded', appInit);
+      //validate the form
+      email.addEventListener('blur', validateField);
+      subject.addEventListener('blur', validateField);
+      message.addEventListener('blur', validateField);
+
+      //sen email & reset button
+      sendEmailForm.addEventListener('submit', sendEmail);
+      resetBtn.addEventListener('click', resetForm);
+}
+
+
+
+/*******************/
+//functions
+/*******************/
+//app Initialization
+function appInit() {
+      sendBtn.disabled = true;
+}
+
+function sendEmail(e) {
+      e.preventDefault();
+      //show the spinner
+      const spinner = document.querySelector('#spinner');
+      spinner.style.display = 'block';
+      //show the image
+      const sendEmailImg = document.createElement('img');
+      sendEmailImg.src = 'img/mail.gif';
+      sendEmailImg.style.display = 'block';
+
+
+      //hide the spinner after 5 sec then show the send email
+      setTimeout(function(){
+            //hide spinner
+            spinner.style.display = 'none';
+
+            //show the image
+            document.querySelector('#loaders').appendChild( sendEmailImg );
+            //hide the image
+           setTimeout(function() {
+            sendEmailForm.reset();
+            sendEmailImg.remove();
+           },5000)
+            
+
+      }, 3000 );
+}
+
+//Validate the fields
+function validateField() {
+      let errors;
+
+      //validate the lenght of the field
+      validateLength(this);
+
+      // validate the email
+      if(this.type === 'email') {
+            validateEmail(this);
+      }
+
+      //Both will return errors then check if there are any errors
+      errors = document.querySelectorAll('.error');
+
+      //check that the inputs are not empty
+      if(email.value !== '' && subject.value !== '' && message.value !== '') {
+            if(errors.length === 0) {
+                  sendBtn.disabled = false;
+            }
+      }
+}
+
+      //validate the lenght of the fields
+      function validateLength(field) {
+            if(field.value.length > 0 ) {
+                  field.style.borderBottomColor = 'green';
+                  field.classList.remove('error');
+             } else {
+                  field.style.borderBottomColor = 'red';
+                  field.classList.add('error');
+            }       
+      }
+
+      //validate enail check for @ in the value
+
+      function validateEmail(field) {
+            let emailText = field.value;
+
+            if (emailText.indexOf('@') !== -1) {
+                  field.style.borderBottomColor = 'green';
+                  field.classList.remove('error');
+             } else {
+                  field.style.borderBottomColor = 'red';
+                  field.classList.add('error');
+            }  
+            
+      }
+
+      //reset the form
+
+      function resetForm() {
+            sendEmailForm.reset();
+      }
