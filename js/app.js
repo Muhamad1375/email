61749d38f84e2912276504314a0a01151cd5d8cc
Muhamad1375 <Mohammadali.fallahzade@gmail.com>
2023-04-04 13:20:04 +0330

    validatinf fields

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 /*******************/
 //variables
 /*******************/
+const sendBtn = document.getElementById('sendBtn'),
+      email = document.getElementById('email'),
+      subject = document.getElementById('subject'),
+      message = document.getElementById('message');
 
 
 
@@ -8,14 +12,29 @@
 /*******************/
 //event listeners
 /*******************/
+eventListeners();
 
+function eventListeners() {
+      //app init
+      document.addEventListener('DOMContentLoaded', appInit);
+
+      //Validate the forms
+      email.addEventListener('blur', validateField);
+      subject.addEventListener('blur', validateField);
+      message.addEventListener('blur', validateField);
+
+}
 
 
 /*******************/
 //functions
 /*******************/
-//app Initialization
 
+//app Initialization
+function appInit() {
+
+      sendBtn.disabled = true;
+}
 
 
       //show the spinner
@@ -33,11 +52,25 @@
          
 
 //Validate the fields
-
-
+function validateField() {
+      let errors;
       //validate the lenght of the field
-
+      validateLength(this);
       // validate the email
+      if(this.type === 'email') {
+            validateEmail(this);
+      }
+}
+//validate the lenght of the field
+function validateLength(field) {
+      if(field.value.length > 0 ){
+            field.style.borderBottomColor = 'green';
+            field.classList.remove('error');
+      }else {
+            field.style.borderBottomColor = 'red';
+            field.classList.add('error');
+      }
+}
       
 
       //Both will return errors then check if there are any errors
@@ -48,8 +81,17 @@
       //validate the lenght of the fields
       
 
-      //validate enail check for @ in the value
-
+//validate email check for @ in the value
+function validateEmail(field) {
+      let emailText = field.value;
+      if(emailText.indexOf('@') !== -1) {
+            field.style.borderBottomColor = 'green';
+            field.classList.remove('error');
+      }else {
+            field.style.borderBottomColor = 'red';
+            field.classList.add('error');
+      }
+}
       
 
       //reset the form
